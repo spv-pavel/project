5194d77b8eab997a9d5cb24f5e7c1b7b6ebec505
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-27 14:04:04 +0300

    D6.2 Знакомство с django.views

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,18 +1,21 @@
-# Импортируем класс, который говорит нам о том,
-# что в этом представлении мы будем выводить список объектов из БД
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
 
 class ProductsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Product
     # Поле, которое будет использоваться для сортировки объектов
     ordering = 'name'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
+    # queryset = Product.objects.filter(price__lt=300).order_by('-name')
     template_name = 'products.html'
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
+    pk_url_kwarg = 'id'
