8d8cd6ba1c87dcc44ea4945806ddf52f63ecfdd3
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-28 13:15:28 +0300

    D6.3

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, DetailView
+from datetime import datetime
+# from pprint import pprint
+
+from django.views.generic import DetailView, ListView
 
 from .models import Product
 
@@ -12,6 +15,22 @@ class ProductsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = "Распродажа в среду!"
+        # pprint(context)
+        return context
 
 
 class ProductDetail(DetailView):
